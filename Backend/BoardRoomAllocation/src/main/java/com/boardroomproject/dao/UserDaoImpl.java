236080b64c6d20d427fd944dba3f31b6a043c44e
Sharanya Mahesheka <sharanya.mahesheka@accolite.com>
2018-01-26 11:35:04 +0530

    Backend Changes 1

--- Backend/BoardRoomAllocation/src/main/java/com/boardroomproject/dao/UserDaoImpl.java
+++ Backend/BoardRoomAllocation/src/main/java/com/boardroomproject/dao/UserDaoImpl.java
@@ -6,6 +6,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
@@ -27,21 +29,28 @@ public class UserDaoImpl implements UserDao{
 		String saveUser = "insert into User (fName,lName,userName,dob,password,contact,address,gender,location,type,isArchived) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 		user.setType("USER");
 		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-		String hashedPassword = passwordEncoder.encode(user.getPassword());
-	    jdbcTemplateObject.update( saveUser, user.getfName(), user.getlName(), user.getDob(), hashedPassword, user.getContact(), user.getAddress(), user.getGender(), user.getLocation(), user.getType(),"N");
+		//String hashedPassword = passwordEncoder.encode(user.getPassword());
+	    jdbcTemplateObject.update( saveUser, user.getfName(), user.getlName(), user.getUserName(),user.getDob(), user.getPassword(), user.getContact(), user.getAddress(), Character.toString(user.getGender()), user.getLocation(), user.getType(),"N");
 	}
 	public void deleteUserById(int id){
-		String deleteUser = "update user set isArchived = ? where id = ?";
+		String deleteUser = "update user set isArchived = ? where userId  = ?";
 	    jdbcTemplateObject.update(deleteUser, "Y", id);
 	}
 	public void updateUser(User user) {
-		 String updateUser = "update user set age = ? where id = ?";
+		 String updateUser = "update user set age = ? where userId  = ?";
 	     jdbcTemplateObject.update(updateUser, user.getUserId());
 	}
 	public boolean isUserExist(User user) {
-		String checkExist = "select 1 from user where id = ? and isArchived = ?";
-		User u =  jdbcTemplateObject.queryForObject(checkExist, new Object[]{user.getUserName(),"N"},new BeanPropertyRowMapper<User>(User.class));
-		return u!=null;
+		String checkExist = "select * from user where userName = ? and isArchived = ?";
+		
+		User u;
+		try {
+			u = jdbcTemplateObject.queryForObject(checkExist, new Object[] { user.getUserName(), "N" },
+					new BeanPropertyRowMapper<User>(User.class));
+		} catch (EmptyResultDataAccessException e) {
+			return false;
+		}
+		return true;
 	}
 	public List<User> getUserByLocation(int lid) {
 		 return jdbcTemplateObject.query("select * from user where lid = ? and isArchived = ?",new RowMapper<User>(){  
@@ -55,18 +64,23 @@ public class UserDaoImpl implements UserDao{
 		    },lid,"N");  
 	}
 	public User getUserById(int userId) {
-		String checkExist = "select 1 from user where id = ? and isArchived = ?";
+		String checkExist = "select 1 from user where userId  = ? and isArchived = ?";
 		return jdbcTemplateObject.queryForObject(checkExist, new Object[]{userId,"N"},new BeanPropertyRowMapper<User>(User.class));
 	}
 	public void changeUserType(int userId) {
-		String updateUser = "update user set type = ? where id = ?";
+		String updateUser = "update user set type = ? where userId  = ?";
 	    jdbcTemplateObject.update(updateUser, "ADMIN", userId);
 	}
 	@Override
 	public User validateUser(User user) {
 		String validateUser = "select * from user where userName=? and password= ? and isArchived = ?";
-		
-		return jdbcTemplateObject.queryForObject(validateUser, new Object[]{user.getUserName(),user.getPassword(),"N"},new BeanPropertyRowMapper<User>(User.class));
+		User ValidUser;
+		try {
+			ValidUser =jdbcTemplateObject.queryForObject(validateUser, new Object[]{user.getUserName(),user.getPassword(),"N"},new BeanPropertyRowMapper<User>(User.class));
+		} catch (EmptyResultDataAccessException e) {
+			return null;
+		}
+		return ValidUser;
 	}
 	
 }
